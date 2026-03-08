f81339362cf1dfbd4a1aa5cb4a2b8253d4b4dd65
Sucipto <sucipto@pm.me>
2026-03-08 09:49:36 +0700

    refactor: rename main module from Maui to PUI

---
+++ lib/pui.ex
@@ -0,0 +1,25 @@
+defmodule PUI do
+  @moduledoc """
+  PUI keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
+  """
+
+  defmacro __using__(_opts) do
+    quote do
+      import PUI
+      import PUI.Input
+      import PUI.Button
+      import PUI.Dropdown
+      import PUI.Alert
+      import PUI.Popover
+      import PUI.Select
+      import PUI.Dialog
+      import PUI.Components
+    end
+  end
+
+  defdelegate popover_base(assigns), to: PUI.Popover, as: :base
+end
