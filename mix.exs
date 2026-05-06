08ec9e7cba82062db3bbd8f00d948158038268a5
Sucipto <sucipto@pm.me>
2026-05-06 21:44:20 +0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PUI.MixProject do
   def project do
     [
       app: :pui,
-      version: "1.0.0-beta.1",
+      version: "1.0.0-beta.2",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
